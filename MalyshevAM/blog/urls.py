cfa9d5ba0534f5a52403e85cfd76644ec997c5fd
MalyshevAM <com.hizik@gmail.com>
2024-10-24 21:06:35 +0300

    сделал практикум 4 - добавление страниц с версткой таблиц

--- MalyshevAM/blog/urls.py
+++ MalyshevAM/blog/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('praktikum3_1/', praktikum3_1),
     path('praktikum3_2/', praktikum3_2),
     path('praktikum3_3/', praktikum3_3),
+    path('praktikum4_1/', praktikum4_1),
+    path('praktikum4_2/', praktikum4_2),
+    path('praktikum4_3/', praktikum4_3),
+    path('praktikum4_4/', praktikum4_4),
 ]
--- MalyshevAM/blog/views.py
+++ MalyshevAM/blog/views.py
@@ -15,3 +15,16 @@ def praktikum3_2(request):
 
 def praktikum3_3(request):
     return render(request, 'blog/praktikum3_3.html')
+
+def praktikum4_1(request):
+    return render(request, 'blog/praktikum4_1.html')
+
+def praktikum4_2(request):
+    return render(request, 'blog/praktikum4_2.html')
+
+def praktikum4_3(request):
+    return render(request, 'blog/praktikum4_3.html')
+
+def praktikum4_4(request):
+    return render(request, 'blog/praktikum4_4.html')
+
