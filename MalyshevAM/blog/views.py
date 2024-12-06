81a57066bb0feb87dc2ce91a992ba74dd1b0297e
MalyshevAM <com.hizik@gmail.com>
2024-12-06 20:46:07 +0300

    поправил баг, теперь все работает, посты отображаются, открыл бд еще

--- MalyshevAM/blog/views.py
+++ MalyshevAM/blog/views.py
@@ -11,7 +11,7 @@ class Home(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница/Classic Blog Design'
+        context['title'] = 'Classic Blog Design'
         return context
 
 # Create your views here.
